88c2eb745165407f548bcb33a360d78f1662aa55
hxq895ft26-spec <hxq895ft26@privaterelay.appleid.com>
2026-02-03 17:53:02 -0600

    Add files via upload

---
+++ EVA_1_PROJECT/build/classes/eva_1_project/EVA_1_PROJECT.rs
@@ -0,0 +1,2 @@
+eva_1_project.Persona
+eva_1_project.EVA_1_PROJECT
---
+++ EVA_1_PROJECT/src/eva_1_project/EVA_1_PROJECT.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_1_project;
+
+/**
+ *
+ * @author 4PF95LA_1909
+ */
+public class EVA_1_PROJECT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //VARIABLES LOCALES: --> STACK
+        //Cualquier variable declarada en un método
+        int x = 100; //4 bytes
+        char caracter = "c"; //2 bytes
+        
+        //VARIABLES DINÁMICAS: --> HEAP
+        //OBJETOS
+        Persona per = new Persona();
+    }
+    
+}
+class Persona{
+    
+}

f92912dff7ea75aee83d0a37f2c19bfdfe4c84b6
hxq895ft26-spec <hxq895ft26@privaterelay.appleid.com>
2026-02-04 17:22:42 -0600

    Add files via upload

---
+++ EVA_3_STACK_OVERFLOW/src/eva_3_stack_overflow/EVA_3_STACK_OVERFLOW.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_3_stack_overflow;
+
+/**
+ *
+ * @author 4PF95LA_1909
+ */
+public class EVA_3_STACK_OVERFLOW {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        A();
+    }
+        public static void A(){
+            B();
+        }
+        public static void B(){
+            A();
+        }
+
+
+    }
+    
+

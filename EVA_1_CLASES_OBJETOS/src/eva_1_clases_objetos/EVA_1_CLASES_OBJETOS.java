99acdb881d4cc94f6717f568dc9b756edab715fe
hxq895ft26-spec <hxq895ft26@privaterelay.appleid.com>
2026-02-04 17:22:10 -0600

    Add files via upload

---
+++ EVA_1_CLASES_OBJETOS/src/eva_1_clases_objetos/EVA_1_CLASES_OBJETOS.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva_1_clases_objetos;
+
+/**
+ *
+ * @author 4PF95LA_1909
+ */
+public class EVA_1_CLASES_OBJETOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ EVA_1_CLASES_OBJETOS/src/eva_1_clases_objetos/Persona.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva_1_clases_objetos;
+
+/**
+ *
+ * @author 4PF95LA_1909
+ */
+class Persona {
+    
+}

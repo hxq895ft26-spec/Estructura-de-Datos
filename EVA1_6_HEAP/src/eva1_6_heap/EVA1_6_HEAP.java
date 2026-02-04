4285baadd2c07e94455a086638ead8d42c785312
hxq895ft26-spec <hxq895ft26@privaterelay.appleid.com>
2026-02-04 17:23:29 -0600

    Add files via upload

---
+++ EVA1_6_HEAP/src/eva1_6_heap/EVA1_6_HEAP.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_6_heap;
+
+/**
+ *
+ * @author 4PF95LA_1909
+ */
+public class EVA1_6_HEAP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i = 100;
+        Object obj = new Object();
+        System.out.print(obj);
+        EVA1_6_HEAP memory = new EVA1_6_HEAP();
+        memory.foo(obj);
+                }
+    public static void foo(Object param){
+        String str = param.
+        System.out.printIn(str);
+                
+    }
+    
+}

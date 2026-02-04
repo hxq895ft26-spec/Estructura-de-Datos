1007f39d059e755f102fbe0c002d1038936b969c
hxq895ft26-spec <hxq895ft26@privaterelay.appleid.com>
2026-02-04 17:21:44 -0600

    Add files via upload

---
+++ EVA_2_STACK/src/eva_2_stack/EVA_2_STACK.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_2_stack;
+
+/**
+ *
+ * @author 4PF95LA_1909
+ */
+public class EVA_2_STACK {
+        public static void main(String[] args) {
+        A();
+    }
+    public static void A(){
+        B();
+    }
+    public static void B(){
+        int i = 5;
+        double b = 10.5;
+    }
+}

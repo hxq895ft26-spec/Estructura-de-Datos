2c6c5e5b4f05e6b670bb6a243953f88f15b43e69
Diego Alfonso Cano Villa <hxq895ft26@privaterelay.appleid.com>
2026-03-11 17:53:34 -0600

    Add files via upload

---
+++ EVA2_2_LISTA_SIMPLE/src/eva2_2_lista_simple/Lista.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package eva2_2_lista_simple;
+
+/**
+ *
+ * @author 4PF95LA_1909
+ */
+public class Lista {
+    
+}

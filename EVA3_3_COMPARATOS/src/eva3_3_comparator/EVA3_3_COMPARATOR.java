2e1a650f94c8c5dfa4e43edb2d4dfe8fb4d6e7c2
Diego Alfonso Cano Villa <hxq895ft26@privaterelay.appleid.com>
2026-05-12 17:11:17 -0600

    Add files via upload

---
+++ EVA3_3_COMPARATOS/src/eva3_3_comparator/EVA3_3_COMPARATOR.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_3_comparatos;
+
+/**
+ *
+ * @author 4PF95LA_1909
+ */
+public class EVA3_3_COMPARATOR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        LinkedList
+    }
+    
+}
